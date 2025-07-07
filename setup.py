0ecda5130629c09c54980fed660b40eafbf23c57
Chen Lin <chenlin@ChendeMacBook-Pro.local>
2025-07-07 15:07:14 -0700

    add module

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+import os
 
 # Read requirements
 with open('requirements.txt') as f:
@@ -7,14 +8,24 @@ with open('requirements.txt') as f:
 # Filter out comments and empty lines
 requirements = [r for r in requirements if r and not r.startswith('#')]
 
+# Find all packages in the repository
+packages = find_packages()
+
+# Make sure we include all directories that contain Python files
+package_dir = {}
+py_modules = ["MACVO"]  # Include the main MACVO.py file as a module
+
 setup(
     name="macvo",
     version="0.1.0",
-    packages=find_packages(),
+    packages=packages,
+    py_modules=py_modules,  # Include standalone Python files
+    package_dir=package_dir,
     install_requires=requirements,
     python_requires=">=3.10",
     description="MAC-VO: Visual Odometry",
     author="MAC-VO Team",
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
